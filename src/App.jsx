24b026287e369a1f03f5fef8bd0f7e3806fcf1ae
Johnathan Pedersen <jpedersen2005@gmail.com>
2024-10-31 20:08:31 -0700

    new css and general fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
 import {puppyList} from './data.js'
 import { useState } from 'react'
-
+import './App.css'
 function App() {
 
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
 
   return (
+    
     <div className="App">
+      <div>      
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}</div>
+
       {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+          return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>})
+          }
     </div>
   );
 }
